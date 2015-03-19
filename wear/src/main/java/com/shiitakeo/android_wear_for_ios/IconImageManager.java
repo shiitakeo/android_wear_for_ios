ca46473d4757e894faa12eff6886679b545ce3cc
kusabuka <kusabuka.takahiro@lab.ntt.co.jp>
2015-03-20 03:10:00 +0900

    add icons. mail, whatsapp, calendar, hangouts

--- wear/src/main/java/com/shiitakeo/android_wear_for_ios/IconImageManager.java
+++ wear/src/main/java/com/shiitakeo/android_wear_for_ios/IconImageManager.java
@@ -4,13 +4,17 @@ package com.shiitakeo.android_wear_for_ios;
  * Created by kusabuka on 15/03/15.
  */
 public class IconImageManager {
+    private static final int app_icon = R.drawable.ic_launcher;
     private static final int line = R.drawable.line;
     private static final int phone = R.drawable.call;
     private static final int facebook = R.drawable.facebook;
     private static final int messanger = R.drawable.messenger;
     private static final int twitter = R.drawable.twitter;
     private static final int gmail = R.drawable.gmail;
-    private static final int app_icon = R.drawable.ic_launcher;
+    private static final int whatsapp = R.drawable.whatsapp;
+    private static final int mail = R.drawable.mail;
+    private static final int calendar = R.drawable.calendar;
+    private static final int hangouts = R.drawable.hangouts;
 
     IconImageManager(){
 
@@ -28,8 +32,18 @@ public class IconImageManager {
                 return line;
             case "facebook":
                 return facebook;
-            case "twitter":
+            case "com.tapbots.Tweetbot":
                 return twitter;
+            case "com.tapbots.Tweetbot3":
+                return twitter;
+            case "net.whatsapp.WhatsApp":
+                return whatsapp;
+            case "com.apple.mobilemail":
+                return mail;
+            case "com.apple.mobilecal":
+                return calendar;
+            case "com.google.hangouts":
+                return hangouts;
             default:
                 return app_icon;
         }
